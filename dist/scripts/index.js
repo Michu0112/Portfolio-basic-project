5d2d7d09f90e298cce7e15dbe6b44c75ab393b6a
Michu0112 <66910540+Michu0112@users.noreply.github.com>
2021-01-15 21:49:28 +0100

    add project start

---
+++ dist/scripts/index.js
@@ -0,0 +1,57 @@
+const li = document.getElementById('drop-list');
+const dropList = document.getElementById('drop-list-sub');
+li.addEventListener('mouseenter', () =>{
+    dropList.style.display = 'flex';
+    setTimeout(() =>{
+        dropList.style.opacity = '1';
+    },1)
+})
+li.addEventListener('mouseleave', () =>{
+    dropList.style.opacity = '0';
+    setTimeout(() =>{
+        dropList.style.display = 'none';
+    },300) 
+})
+const banerText = document.getElementById('banner-first-text');
+const newCollectionBtn = document.getElementById('pseudo-btn');
+const startAnimation = (element) =>{
+    let delay = 0;
+    if(element === newCollectionBtn){
+        delay = 200;
+    }
+    setTimeout(() =>{
+        element.style.transform = 'translateY(-50px)';
+        element.style.opacity = '1';
+    },delay)
+}
+startAnimation(banerText);
+startAnimation(newCollectionBtn);
+
+
+
+let i = 0;
+
+const newCollectionArticles = document.querySelectorAll('.first-page-product');
+
+function callbackFunc(entries, observer)
+{
+  entries.forEach( (el) => {
+    i++;
+    if(i>3){
+    el.target.style.transform = `translateY(-50px)`;
+    el.target.style.opacity = `1`;
+    }
+  });
+}
+
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+  };
+
+let observer = new IntersectionObserver(callbackFunc, options);
+
+newCollectionArticles.forEach( el =>{
+    observer.observe(el);
+})
