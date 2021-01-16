58722f9cda2bcdd17228ef9fc3c5a721ac6237ee
Michu0112 <66910540+Michu0112@users.noreply.github.com>
2021-01-16 15:32:55 +0100

    fix layout brake in contact

--- dist/scripts/animationContact.js
+++ dist/scripts/animationContact.js
@@ -1,7 +1,7 @@
 const content = document.getElementById('content');
 const animFunc = () =>{
     setTimeout(() =>{
-        content.style.transform = 'translateY(50px)';
+        content.style.transform = 'translateY(-10px)';
         content.style.opacity = '1';
     },200);
 }
