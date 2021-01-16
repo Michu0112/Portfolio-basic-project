1de5ec0c3ab943c55f180482cb2867440872c187
Michu0112 <66910540+Michu0112@users.noreply.github.com>
2021-01-16 04:27:01 +0100

    add function initialization in contact animation

--- dist/scripts/animationContact.js
+++ dist/scripts/animationContact.js
@@ -1,3 +1,6 @@
 const content = document.getElementById('content');
-content.style.transform = 'translateY(-50px)';
-content.style.opacity = '1';
+const animFunc = () =>{
+    content.style.transform = 'translateY(-50px)';
+    content.style.opacity = '1';
+}
+animFunc();
